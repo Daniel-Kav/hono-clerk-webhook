00ae69b8dfff0d3bd76353c53d4c13cc14e731db
daniel-kav <dankavatha01@gmail.com>
2023-07-01 12:12:13 +0300

    adding validators

---
+++ main.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form');
+const firstname = document.getElementById('first_name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirm_password = document.getElementById('confrim_password');
